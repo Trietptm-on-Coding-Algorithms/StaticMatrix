12b4936574630fab67134cb9982636c01bc12026
zerosum0x0 <zerosum0x0@gmail.com>
2014-07-18 12:33:24 -0600

    initial commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "staticmatrix.h"
+
+using namespace std;
+
+int main()
+{
+    Zero::StaticMatrix<double> mat(10, 10);
+    mat[0][0] = 1.5f;
+    mat[9][9] = 1.4f;
+
+    std::cout << mat[0][0] << std::endl;
+    std::cout << mat[9][9] << std::endl;
+
+    return 0;
+}
+
